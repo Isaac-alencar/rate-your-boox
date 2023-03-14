817f1f062f8ff17bda3fed6410938a433a7604eb
Isaac-alencar <alencarisaac3@gmail.com>
2023-03-14 15:38:22 -0300

    add username field to signup form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,15 @@
 
 # ApplicationController
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   protected
 
   def after_sign_in_path_for(resource)
     stored_location_for(resource) || user_path(current_user.id)
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
   end
 end
