c1447b601cc9459a1e8684701fc8a38b9e4c05ca
Isaac-alencar <alencarisaac3@gmail.com>
2023-03-29 12:00:08 -0300

    fix home controller spec

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -4,12 +4,11 @@ require 'rails_helper'
 
 # HomeControllerSpec
 RSpec.describe HomeController, type: :request do
-  # index
   describe 'GET /' do
-    # it 'renders homepage' do
-    #   get root_url
-    #   expect(response).to be_successful
-    #   expect(response).to have_http_status(200)
-    # end
+    it 'renders homepage' do
+      get root_url
+      expect(response).to be_successful
+      expect(response).to have_http_status(200)
+    end
   end
 end
